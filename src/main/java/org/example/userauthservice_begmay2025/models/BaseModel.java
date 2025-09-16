20dab8c1d589c7ad5bd65f594a6a07cb79c0e1d1
Ninad1991 <nakhwaninad@gmail.com>
2025-09-16 22:36:48 +0530

    initial commit to add project to the repo

---
+++ src/main/java/org/example/userauthservice_begmay2025/models/BaseModel.java
@@ -0,0 +1,17 @@
+package org.example.userauthservice_begmay2025.models;
+
+import java.util.Date;
+
+public abstract class  BaseModel {
+
+    private Long id;
+
+    private Date createdAt;
+
+    private Date lastUpdatedAt;
+
+//    private String createdBy;
+
+    private State state;
+
+}
---
+++ src/main/java/org/example/userauthservice_begmay2025/models/User.java
@@ -0,0 +1,4 @@
+package org.example.userauthservice_begmay2025.models;
+
+public class User {
+}
